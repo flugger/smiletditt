e552f9075fb174159a5ffcf0dac00605c441d9c8
Flugger <flugged@gmail.com>
2014-10-01 17:13:59 +0200

    Updated buttons again

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,11 @@
 $('.date').pickmeup();
 
 $( '.js-departmentButton' ).click( function() {
-    $( this).parents( 'ul' ).prev().html( 'Avdeling ' + $( this).text() + '. <a href = "#" class = "BrandIntro-link">Endre?</a>' );
-    $( this).parents( 'ul').hide().next().css( 'display', 'block' );
+    $( this ).parents( 'ul' ).prev().html( 'Avdeling ' + $( this).text() + '. <a href = "#" class = "BrandIntro-link js-changeDepartment">Endre?</a>' );
+    $( this ).parents( 'ul').hide().next().css( 'display', 'block' );
+});
+
+$( '.BrandIntro-wrap' ).on( 'click', '.js-changeDepartment', function() {
+    $( this ).parent().next().show().next().hide();
+    $( this ).parent().html( 'Vennligst velg avdeling:' );
 });
