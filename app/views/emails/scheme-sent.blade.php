33d680f03f3d72b33a22638a1b59fd67c2ed1cf1
Flugger <flugged@gmail.com>
2014-11-18 18:57:37 +0100

    Email thumbnail.

--- app/views/emails/scheme-sent.blade.php
+++ app/views/emails/scheme-sent.blade.php
@@ -1,9 +1,40 @@
 <!DOCTYPE html>
-<html lang="en-US">
+<html lang="no">
 	<head>
-		<meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Smilet Ditt</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	</head>
 	<body>
-		<h2>Takk skal du være</h2>
-	</body>
+<body style="font-family:'Open Sans',Helvetica,Arial,sans-serif !important; margin: 0 auto; padding: 0; color:#333 !important">
+ <table border="0" cellpadding="0" cellspacing="0" style="font-family:'Open Sans',Helvetica,Arial,sans-serif !important; font-weight: 200;height:100%!important;margin:0;padding:0;width:100%!important;font-size:16px; color:#333 !important">
+     <tr>
+    <td align="center" style="font-family:'Open Sans',Helvetica,Arial,sans-serif; font-size: 14px; color: #333;">
+     <table align="center" border="0" cellpadding="0" cellspacing="0" style="font-family:'Open Sans',Helvetica,Arial,sans-serif; color:#333; max-width: 600px; width: 90%; display: block">
+     <tr>
+      <td align="center" style="padding: 0;">
+        <img src="http://smiletditt.no/images/email-thumbnail.jpg" alt="Smilet Ditt" style="display: block; width: 600px; height: auto;" />
+      </td>
+     </tr>
+     <tr>
+      <td bgcolor = "#e8eff2" style = "font-family:'Open Sans',Helvetica,Arial,sans-serif; color:#333; padding: 30px 7%">
+       <h1 style = "margin-top: 0px; font-family:'Open Sans',Helvetica,Arial,sans-serif; color:#333; font-weight: 200;"><span style = "font-family:'Open Sans',Helvetica,Arial,sans-serif; color:#333">Kontaktskjema Sendt</span></h1>
+       <p>
+       <div style = "font-family:'Open Sans',Helvetica,Arial,sans-serif; color:#333">
+        Takk for din henveldelse, vi vil kontakte deg så snart som mulig.
+       </div>
+       </p>
+       <p>
+       <div style = "font-family:'Open Sans',Helvetica,Arial,sans-serif; color:#333">
+        Med vennlig hilsen,<br/>
+        Smilet Ditt Tannklinikk
+       </div>
+       </p>
+      </td>
+     </tr>
+    </table>
+      </td>
+     </tr>
+ </table>
+</body>
 </html>
