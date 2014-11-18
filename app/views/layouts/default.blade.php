9b372bbe43ae32d08a77566301736db33489a306
Flugger <flugged@gmail.com>
2014-11-18 19:31:48 +0100

    Yes.

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -203,6 +203,15 @@
             </footer>
         </section>
 
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-56838502-1');ga('send','pageview');
+        </script>
+
         {{ HTML::script( '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js' ) }}
         {{ HTML::script( 'js/jquery.pickmeup.min.js' ) }}
         {{ HTML::script( 'js/jquery.icheck.min.js' ) }}
